e421882ae0971d9a272e1babb8fc1009f84dfc9e
Elgun1234 <112864674+Elgun1234@users.noreply.github.com>
2022-12-13 10:45:31 +0000

    Update utils.py

--- valorant_api/utils.py
+++ valorant_api/utils.py
@@ -10,3 +10,30 @@ def scrape(url: str):
     mystr = mybytes.decode("utf8")
     fp.close()
     return json.loads(mystr)
+
+def username_check(username):
+    f = open("accounts.txt", "r")
+    data = f.read()
+    if username in data:
+        return False
+    else:
+        return True
+
+
+def add_account(username,pass1):
+    f = open("accounts.txt", "+")
+    f.write(f"{username}  {pass1} \n \n")
+
+def number_check(pass1):
+    k=0
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    for i in numbers:
+        if i in pass1:
+            k+=1
+    if k>=3:
+        return True
+    else:
+        return False
+
+
+
